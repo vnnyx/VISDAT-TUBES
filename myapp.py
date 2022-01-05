7cc311001dabba7f40e4b24476a3107640135ed5
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-01-05 23:46:32 +0700

    fix bug

--- myapp.py
+++ myapp.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
+from os.path import dirname, join
 from controller.covid import *
 from bokeh.io import curdoc
 from bokeh.io import show, output_file, save
 
 output_file("covid.html")
-df = pd.read_csv("data/covid.csv")
+df = pd.read_csv(join(dirname(__file__), "data", "covid.csv"))
 df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
 layout = covid(df)
 save(layout)
