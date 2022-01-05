89f7d3d12fa73f66414ad0442fe140cfb8ea8fbc
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-01-05 22:56:22 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+from controller.covid import *
+from bokeh.io import curdoc
+from bokeh.io import show, output_file, save
+
+output_file("covid.html")
+df = pd.read_csv("dataset/covid.csv")
+df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
+layout = covid(df)
+save(layout)
+
+# curdoc().add_root(layout)
+# curdoc().title = "Application"
+
+
+# show(curdoc)
