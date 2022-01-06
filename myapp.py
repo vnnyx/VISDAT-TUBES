11601a0a6478f945d8657cddfb4d2cd00d712357
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-01-06 08:59:44 +0700

    add some comment

--- myapp.py
+++ myapp.py
@@ -4,11 +4,13 @@ from os.path import dirname, join
 from script.covid import *
 from bokeh.io import curdoc
 
-
+# Membaca file csv
 df = pd.read_csv(join(dirname(__file__), "data", "covid.csv"))
+
+# Mengubah datatype untuk kolom date menjadi datetime
 df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
 layout = covid(df)
 
-
+# App configuration
 curdoc().add_root(layout)
-curdoc().title = "Kasus Covid-19 Dunia"
+curdoc().title = "Kasus COVID-19 Dunia"
--- script/covid.py
+++ script/covid.py
@@ -13,19 +13,23 @@ from bokeh.layouts import column, row
 
 
 def covid(df):
+    # Membuat data source
     def create_dataset(df):
         return ColumnDataSource(data=df)
 
+    # Membuat line plot untuk kasus harian
     def daily_plot(src):
+        # Membuat model plot
         p = figure(
             plot_width=1000,
             plot_height=700,
             x_axis_type="datetime",
-            title="Kasus Covid-19 Harian",
+            title="Kasus COVID-19 Harian",
             x_axis_label="Tanggal",
             y_axis_label="Kasus baru",
         )
 
+        # Membuat line plot
         pm_line = p.line(
             source=src,
             x="date",
@@ -36,32 +40,38 @@ def covid(df):
             name="pm_line",
         )
 
+        # Menambahkan hover
         hover = HoverTool(
             tooltips=[
                 ("Tanggal", "@date{%F}"),
                 ("Kasus baru", "@daily_new_cases"),
-                ("Total case", "@cumulative_total_cases"),
+                ("Total kasus", "@cumulative_total_cases"),
             ],
             formatters={"@date": "datetime"},
             mode="vline",
         )
 
+        p.add_tools(hover)
+
+        # Menambahkan style plot
         p.title.align = "center"
         p.title.text_font_size = "16pt"
 
-        p.add_tools(hover)
         return p
 
+    # Membuat line plot untuk kasus kematian
     def death_plot(src):
+        # Membuat model plot
         p = figure(
             plot_width=1000,
             plot_height=700,
             x_axis_type="datetime",
-            title="Kematian Covid-19 Harian",
+            title="Kematian COVID-19 Harian",
             x_axis_label="Tanggal",
             y_axis_label="Kematian baru",
         )
 
+        # Membuat line plot
         pm_line = p.line(
             source=src,
             x="date",
@@ -72,56 +82,60 @@ def covid(df):
             name="pm_line",
         )
 
+        # Menambahkan hover
         hover = HoverTool(
             tooltips=[
                 ("Tanggal", "@date{%F}"),
                 ("Meninggal", "@daily_new_deaths"),
-                ("Total", "@cumulative_total_deaths"),
+                ("Total meninggal", "@cumulative_total_deaths"),
             ],
             formatters={"@date": "datetime"},
             mode="vline",
         )
 
+        p.add_tools(hover)
+
+        # Menambahkan style plot
         p.title.align = "center"
         p.title.text_font_size = "16pt"
-        p.add_tools(hover)
+
         return p
 
+    # Membuat callback function untuk update plot
     def update_plot(attr, old, new):
         country = select.value
         new_df = df[df["country"] == country]
         new_src = create_dataset(new_df)
         src.data.update(new_src.data)
 
-    # Init PLot
+    # Init plot
     src = create_dataset(df[df["country"] == "Indonesia"])
     p_daily = daily_plot(src)
     p_death = death_plot(src)
 
-    # Add Dropdown Select
+    # Menambahkan dropdown select
     select = Select(
         options=list(df["country"].unique()), value="Indonesia", title="Country"
     )
     select.on_change("value", update_plot)
 
-    # Add color picker
+    # Menambahkan color picker
     pm_line = p_daily.select_one({"name": "pm_line"})
     pm_line2 = p_death.select_one({"name": "pm_line"})
     picker = ColorPicker(title="Line Color", color="firebrick")
     picker.js_link("color", pm_line.glyph, "line_color")
     picker.js_link("color", pm_line2.glyph, "line_color")
 
-    # Set layout
+    # Set Layout
     controller1 = column(picker, select)
     controller2 = column(picker, select)
     layout = row([p_daily, controller1])
     layout2 = row([p_death, controller2])
 
-    # Add Tab
+    # Menambahkan tab
     tab1 = Panel(child=layout, title="Kasus Harian")
     tab2 = Panel(child=layout2, title="Kematian")
 
     tab = Tabs(tabs=[tab1, tab2])
 
-    # layout = column(tab)
     return tab
